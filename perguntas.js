474faa851bece2eb268e2e4e6a73acc56c296b7f
fabischor <103068339+fabischor@users.noreply.github.com>
2025-07-30 08:05:38 -0300

    Perguntas

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao (
+    'Matemática',
+    'O que é o número Pi?',
+    'O número Pi é um número irracional, obtido pela divisão do comprimento da circunferência pelo seu diâmetro.'
+)
+
+criaCartao (
+    'Matemática',
+    'O que são números racionais?',
+    'São números que podem ser escritos na forma de fração a/b, em que a é diferente de 0.'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual é a capital do Paraná?',
+    'A capital do Paraná é Curitiba.'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual é a capital do Brasil?',
+    'A capital do Brasil é Brasília.'
+)
