dd9fd0b1c81318114c0f698fc953ad5c55300c05
fabischor <103068339+fabischor@users.noreply.github.com>
2025-08-13 07:37:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,14 @@ function criaCartao(categoria, pergunta, resposta) {
   cartao.className = 'cartao'
 
   cartao.innerHTML = `
+  <div class="cartao__conteudo">
+    <h3>Matemática</h3>
+   <div class="cartao__conteudo__pergunta">
+   <p>O que é o número Pi?</p>
+   </div>
+  <div class="cartao__conteudo__resposta">
+   <p>O número Pi é um número irracional, obtido pela divisão do comprimento da circunferência pelo seu diâmetro.</p>
+   </div>
+   </div>
+   `
 }
